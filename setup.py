4be6936cc1fd715fcffdd4269b9b71d444a28b6c
safarma1 <114674690+safarma1@users.noreply.github.com>
2023-09-04 21:11:08 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,11 @@
+from setuptools import setup, find_packages
 
+with open('https://raw.githubusercontent.com/safarma1/Programko2/semestral/requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='skymap',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=requirements,  # Použití seznamu z requirements.txt
+)
